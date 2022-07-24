233be67ae66664571f3896c9da8ab192c9c80983
chottedifs <husadif.aldi@gmail.com>
2022-07-24 16:57:15 +0700

    update cloudinary

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,6 @@
 module.exports = [
   'strapi::errors',
-  'strapi::security',
+  // 'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
@@ -9,4 +9,18 @@ module.exports = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 ];
